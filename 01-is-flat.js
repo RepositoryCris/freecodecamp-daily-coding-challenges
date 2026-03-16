084e7da06b5f551f5edf6bf32dfe9d80c7a4cb43
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-03-15 23:13:53 -0400

    remove old file from parent directory

--- 01-is-flat.js
+++
@@ -1,9 +0,0 @@
-function isFlat(arr) {
-  return !arr.some(Array.isArray);
-}
-
-console.log(isFlat([1, 2, 3, 4]));
-console.log(isFlat([1, [2, 3], 4]));
-console.log(isFlat([1, 0, false, true, "a", "b"]));
-console.log(isFlat(["a", [0], "b", true]));
-console.log(isFlat([1, [2, [3, [4, [5]]]], 6]));
