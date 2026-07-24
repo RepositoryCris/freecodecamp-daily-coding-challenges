3fa0fc51f4b1a74ff4b8bb93cc171be1df2dfb86
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-07-24 00:38:56 -0400

    July 21 word blender

--- 2026-07/21-word-blender.js
+++ 2026-07/21-word-blender.js
@@ -1,5 +1,19 @@
 function blendWords(word1, word2) {
-  return word1;
+  const w1 = Math.floor(word1.length / 2);
+  //console.log(w1);
+
+  const w2 = Math.floor(word2.length / 2);
+  //console.log(w2);
+
+  const half1 = word1.slice(0, w1);
+  //console.log(half1);
+
+  const half2 = word2.slice(w2);
+  //console.log(half2);
+
+  const blend = half1.concat(half2);
+
+  return blend;
 }
 
 console.log(blendWords("turtle", "toucan")); // should return "turcan".
