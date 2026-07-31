faa44e208b2e29b325e94e1c1cd053fdad74e227
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-07-31 04:09:25 -0400

    July 31 base of morse code

---
+++ 2026-07/31-morse-code.js
@@ -0,0 +1,35 @@
+function decodeMorse(code) {
+  return code;
+}
+
+console.log(decodeMorse("--..")); // should return "Z".
+console.log(decodeMorse("... --- ...")); // should return "SOS".
+console.log(decodeMorse("..-. .-. . . -.-. --- -.. . -.-. .- -- .--.")); // should return "FREECODECAMP".
+console.log(decodeMorse(".... . .-.. .-.. ---   .-- --- .-. .-.. -..")); // should return "HELLO WORLD".
+console.log(
+  decodeMorse(
+    "- .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. . -..   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --.",
+  ),
+); // should return "THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG".
+
+/*
+Morse Code
+Given a Morse code string, return the decoded message using the following table:
+
+Code	Letter	Code	Letter
+.-	A	-.	N
+-...	B	---	O
+-.-.	C	.--.	P
+-..	D	--.-	Q
+.	E	.-.	R
+..-.	F	...	S
+--.	G	-	T
+....	H	..-	U
+..	I	...-	V
+.---	J	.--	W
+-.-	K	-..-	X
+.-..	L	-.--	Y
+--	M	--..	Z
+Letters are separated by a single space
+Words are separated by three spaces
+ */
