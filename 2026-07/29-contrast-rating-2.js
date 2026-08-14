5ae2dd9ffac81f5b151197c2a1298b25d4741662
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-08-14 03:07:30 -0400

    July 29 contrast rating 2

--- 2026-07/29-contrast-rating-2.js
+++ 2026-07/29-contrast-rating-2.js
@@ -1,6 +1,40 @@
 function getContrastRating(l1, l2, isLargeText) {
-  return l1;
+  let contrastRatio1 = Number(l1);
+  let contrastRatio2 = Number(l2);
+
+  if (isNaN(contrastRatio1) || contrastRatio1 < 0) {
+    return "invalid input";
+  }
+
+  if (isNaN(contrastRatio2) || contrastRatio2 < 0) {
+    return "invalid input";
+  }
+
+  // Add 0.05 to each
+  contrastRatio1 += 0.05;
+  contrastRatio2 += 0.05;
+
+  // Divide the lighter (larger) by the darker (smaller)
+  const contrastRatio =
+    Math.max(contrastRatio1, contrastRatio2) /
+    Math.min(contrastRatio1, contrastRatio2);
+
+  const thresholds = isLargeText
+    ? { AAA: 4.5, AA: 3.0 }
+    : { AAA: 7.0, AA: 4.5 };
+
+  if (contrastRatio >= thresholds.AAA) return "AAA";
+  if (contrastRatio >= thresholds.AA) return "AA";
+
+  return "Fail";
 }
+
+console.log(getContrastRating(1.0, 0.0, false)); // should return "AAA".
+console.log(getContrastRating(0.9015, 0.1364, false)); // should return "AA".
+console.log(getContrastRating(0.8965, 0.1628, false)); // should return "Fail".
+console.log(getContrastRating(0.7469, 0.0957, true)); // should return "AAA".
+console.log(getContrastRating(0.7489, 0.2018, true)); // should return "AA".
+console.log(getContrastRating(0.6571, 0.1974, true)); // should return "Fail".
 
 /*
 Contrast Rating 2
@@ -15,10 +49,3 @@ Rating	Normal Text	Large Text
 "AA"	4.5+	3.0+
 "Fail"	below 4.5	below 3.0
 */
-
-console.log(getContrastRating(1.0, 0.0, false)); // should return "AAA".
-console.log(getContrastRating(0.9015, 0.1364, false)); // should return "AA".
-console.log(getContrastRating(0.8965, 0.1628, false)); // should return "Fail".
-console.log(getContrastRating(0.7469, 0.0957, true)); // should return "AAA".
-console.log(getContrastRating(0.7489, 0.2018, true)); // should return "AA".
-console.log(getContrastRating(0.6571, 0.1974, true)); // should return "Fail".
