d0646e7225c5349ff327dbaf03b6797bd19967bf
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-08-16 22:48:09 -0400

    August 14 base of spacejam

---
+++ 2026-08/14-spacejam.js
@@ -0,0 +1,16 @@
+function spaceJam(s) {
+  return s;
+}
+
+console.log(spaceJam("freeCodeCamp")); // should return "F  R  E  E  C  O  D  E  C  A  M  P".
+console.log(spaceJam("   free   Code   Camp   ")); // should return "F  R  E  E  C  O  D  E  C  A  M  P".
+console.log(spaceJam("Hello World?!")); // should return "H  E  L  L  O  W  O  R  L  D  ?  !".
+console.log(spaceJam("C@t$ & D0g$")); // should return "C  @  T  $  &  D  0  G  $".
+console.log(spaceJam("allyourbase")); // should return "A  L  L  Y  O  U  R  B  A  S  E".
+
+/*
+S P A C E J A M
+Given a string, remove all spaces from the string, insert two spaces between every character, convert all alphabetical letters to uppercase, and return the result.
+
+Non-alphabetical characters should remain unchanged (except for spaces).
+*/
