43a3a57a7e4511b96c3cecb123239c5498bd61f2
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-08-20 17:43:49 -0400

    August 20 base of three strikes

---
+++ 2026-08/20-three-strikes.js
@@ -0,0 +1,14 @@
+function squaresWithThree(n) {
+  return n;
+}
+
+console.log(squaresWithThree(1)); // should return 0.
+console.log(squaresWithThree(10)); // should return 1.
+console.log(squaresWithThree(100)); // should return 19.
+console.log(squaresWithThree(1000)); // should return 326.
+console.log(squaresWithThree(10000)); // should return 4531.
+
+/*
+3 Strikes
+Given an integer between 1 and 10,000, return a count of how many numbers from 1 up to that integer whose square contains at least one digit 3.
+*/
