b8b96725b5bff6478d48853e9072f8457991d866
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-09-13 02:15:51 -0400

    September 13 missing numbers

---
+++ 2026-09/13-missing-numbers.js
@@ -0,0 +1,32 @@
+function findMissingNumbers(arr) {
+  if (arr.length === 0) return [];
+
+  const seen = new Set(arr);
+
+  let max = 0;
+  for (const n of arr) if (n > max) max = n;
+
+  const missingNumbers = [];
+  for (let i = 1; i <= max; i++) {
+    if (!seen.has(i)) missingNumbers.push(i);
+  }
+  return missingNumbers;
+}
+
+console.log(findMissingNumbers([1, 3, 5])); // should return [2, 4].
+console.log(findMissingNumbers([1, 2, 3, 4, 5])); // should return [].
+console.log(findMissingNumbers([1, 10])); // should return [2, 3, 4, 5, 6, 7, 8, 9].
+console.log(findMissingNumbers([10, 1, 10, 1, 10, 1])); // should return [2, 3, 4, 5, 6, 7, 8, 9].
+console.log(findMissingNumbers([3, 1, 4, 1, 5, 9])); // should return [2, 6, 7, 8].
+console.log(
+  findMissingNumbers([1, 2, 3, 4, 5, 7, 8, 9, 10, 12, 6, 8, 9, 3, 2, 10, 7, 4]),
+); // should return [11].
+
+/*
+Missing Numbers
+Given an array of integers from 1 to n, inclusive, return an array of all the missing integers between 1 and n (where n is the largest number in the given array).
+
+The given array may be unsorted and may contain duplicates.
+The returned array should be in ascending order.
+If no integers are missing, return an empty array.
+*/
